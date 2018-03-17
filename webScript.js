5c86682278903a78153e39a01c86a121b5260daf
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2018-03-17 19:34:36 -0300

    a ver...?

--- webScript.js
+++ webScript.js
@@ -1,6 +1,6 @@
-function get(username,password, mail){
+function post(username,password, mail){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://c1536939.ngrok.io/Portal_war_exploded/addUser/{\"username\":\""+ username.value + "\",\"password\":\"" +  password.value + "\",\"mail\":\"" +mail.value + "\",\"examenes\":[]}", false);
+    xhr.open('POST', "http://c1536939.ngrok.io/Portal_war_exploded/addUser/{\"username\":\""+ username.value + "\",\"password\":\"" +  password.value + "\",\"mail\":\"" +mail.value + "\",\"examenes\":[]}", false);
     xhr.send();
     console.log("get");
 }
