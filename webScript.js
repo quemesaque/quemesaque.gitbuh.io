b506a1f2582e725fe750eac2ed4005189cf898f2
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2018-03-17 20:47:04 -0300

    a ver...?

--- webScript.js
+++ webScript.js
@@ -1,6 +1,6 @@
 function post(username,password, mail){
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', "http://c1536939.ngrok.io/Portal_war_exploded/addUser/{\"username\":\""+ username.value + "\",\"password\":\"" +  password.value + "\",\"mail\":\"" +mail.value + "\",\"examenes\":[]}", false);
+    xhr.open('POST', "http://matiasmiodosky.localhost.run/Portal_war_exploded/postUser/{\"username\":\""+ username.value + "\",\"password\":\"" +  password.value + "\",\"mail\":\"" +mail.value + "\",\"examenes\":[]}", false);
     xhr.send();
     console.log("get");
 }
